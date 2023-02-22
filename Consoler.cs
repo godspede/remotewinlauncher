9a22868b057509c54789d9cbf7efb1cad5537de5
Jehu <jehutheawesome@gmail.com>
2023-02-21 19:56:08 -0700

    start as console app, more routing

--- Consoler.cs
+++ Consoler.cs
@@ -1,31 +1,31 @@
 ﻿using System.Runtime.InteropServices;
 
-namespace remotewinlauncher
+/// <summary>
+/// Useful with <OutputType>WinExe</OutputType> in csproj
+/// </summary>
+static class Consoler
 {
-    static class Consoler
+    [DllImport("kernel32.dll", EntryPoint = "GetStdHandle", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr GetStdHandle(int nStdHandle);
+
+    [DllImport("kernel32.dll", EntryPoint = "AllocConsole", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern int AllocConsole();
+
+    private const int STD_OUTPUT_HANDLE = -11;
+
+    private static bool HasShown = false;
+    public static void ShowConsole()
     {
-        [DllImport("kernel32.dll", EntryPoint = "GetStdHandle", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-        public static extern IntPtr GetStdHandle(int nStdHandle);
-
-        [DllImport("kernel32.dll", EntryPoint = "AllocConsole", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-        public static extern int AllocConsole();
-
-        private const int STD_OUTPUT_HANDLE = -11;
-
-        private static bool HasShown = false;
-        public static void ShowConsole()
-        {
-            if (HasShown)
-                return;
-            AllocConsole();
-            IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-            Microsoft.Win32.SafeHandles.SafeFileHandle safeFileHandle = new Microsoft.Win32.SafeHandles.SafeFileHandle(stdHandle, true);
-            FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
-            System.Text.Encoding encoding = System.Text.Encoding.Default;
-            var standardOutput = new StreamWriter(fileStream, encoding);
-            standardOutput.AutoFlush = true;
-            Console.SetOut(standardOutput);
-            HasShown = true;
-        }
+        if (HasShown)
+            return;
+        AllocConsole();
+        IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+        Microsoft.Win32.SafeHandles.SafeFileHandle safeFileHandle = new Microsoft.Win32.SafeHandles.SafeFileHandle(stdHandle, true);
+        FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
+        System.Text.Encoding encoding = System.Text.Encoding.Default;
+        var standardOutput = new StreamWriter(fileStream, encoding);
+        standardOutput.AutoFlush = true;
+        Console.SetOut(standardOutput);
+        HasShown = true;
     }
 }
