0a0182877961ec3bfab8c8a0340219728c87ec30
godspede <jehutheawesome@gmail.com>
2023-02-21 18:04:05 -0700

    init

---
+++ Consoler.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace remotewinlauncher
+{
+    static class Consoler
+    {
+        [DllImport("kernel32.dll", EntryPoint = "GetStdHandle", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll", EntryPoint = "AllocConsole", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int AllocConsole();
+
+        private const int STD_OUTPUT_HANDLE = -11;
+
+        private static bool HasShown = false;
+        public static void ShowConsole()
+        {
+            if (HasShown)
+                return;
+            AllocConsole();
+            IntPtr stdHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+            Microsoft.Win32.SafeHandles.SafeFileHandle safeFileHandle = new Microsoft.Win32.SafeHandles.SafeFileHandle(stdHandle, true);
+            FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
+            System.Text.Encoding encoding = System.Text.Encoding.Default;
+            var standardOutput = new StreamWriter(fileStream, encoding);
+            standardOutput.AutoFlush = true;
+            Console.SetOut(standardOutput);
+            HasShown = true;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # remotewinlauncher
+
+WIP
